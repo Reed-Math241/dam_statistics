9c12d5f445a642d9e97a766523821b027a064be7
Lauren Rabe <lrabe@reed.edu>
2021-04-14 15:27:13 -0700

    made checkboxes work

--- shinyapp.R
+++ shinyapp.R
@@ -3,15 +3,10 @@ library(shiny)
 library(tidyverse)
 
 #generate data
-purpose <- c("Hydroelectricity", "Irrigation", "Water Supply")
-
 test_dat <- data.frame(x = c(5, 2, 3),
                        y = c(7, 4, 6),
                        dam = c("Hydroelectricity",
                                "Irrigation", "Water Supply"))
-
-plot <- ggplot(test_dat, aes(x = x, y = y, color = dam)) +
-  geom_point(size = 2)
 
 
 # User interface
@@ -20,18 +15,15 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       checkboxGroupInput(inputId = "purpose", 
-                         label = h3("Dam purpose"), 
-                         choices = list("Hydroelectricity" = 1,
-                                        "Irrigation" = 2, 
-                                        "Water Supply" = 3),
-                         selected = 1:3),
-      
-      
-      hr(),
-      fluidRow(column(3, verbatimTextOutput("value")))
-    ),
+                         label = "Dam purpose", 
+                         choices = c("Hydroelectricity",
+                                     "Irrigation", "Water Supply"),
+                         selected = c("Hydroelectricity",
+                                      "Irrigation", "Water Supply")
+                         )),
+    
     mainPanel(
-      plotOutput(outputId = "plot")
+      plotOutput("plot")
     )
   )
 )
@@ -39,16 +31,22 @@ ui <- fluidPage(
 
 # Server function
 server <- function(input, output){
+  
+  test_dat2 <- reactive({
+    test_dat %>%
+      filter(dam %in% input$purpose)
+  })
+  
+  
+  
   output$plot <- renderPlot({
-    test_dat <- reactive({data.frame(x = c(5, 2, 3),
-                           y = c(7, 4, 6),
-                           dam = c("Hydroelectricity",
-                                   "Irrigation", "Water Supply"))
-    })
     
-    ggplot(test_dat(), aes(x = x, y = y, 
-                         color = .data[[input$dam]])) +
-      geom_point(size = 2)
+    ggplot(test_dat2(), aes(x = x, y = y, 
+                            color = dam)) +
+      geom_point(size = 2) +
+      scale_color_manual(breaks = c("Hydroelectricity",
+                                    "Irrigation", "Water Supply"),
+                         values = c("#00ba38", "#c77cff", "#619cff"))
   })
 }
 
