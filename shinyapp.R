05f44d2d315a1cb6ac9c780ea91d7b967cf80f35
Lauren Rabe <lrabe@reed.edu>
2021-04-14 18:30:42 -0700

    fixed units!

--- shinyapp.R
+++ shinyapp.R
@@ -1,7 +1,9 @@
 #load libraries
 library(shiny)
 library(tidyverse)
+library(lubridate)
 library(RColorBrewer)
+library(leaflet)
 
 #wrangle data
 dam2015fin2 <- dam2015fin %>%
@@ -16,31 +18,43 @@ dam2015fin2 <- dam2015fin %>%
 # User interface
 ui <- fluidPage(
   titlePanel(title = "Dam dashboard"),
-  sidebarLayout(
-    sidebarPanel(
-      checkboxGroupInput(inputId = "damtype",
-                         label = "Dam use",
-                         choices = levels(dam2015fin2$purpose),
-                         selected = levels(dam2015fin2$purpose)),
-      dateRangeInput(inputId = "dates",
-                     label = "Select date range",
-                     start = as_date("2015-01-01"), end = as_date("2015-12-01"),
-                     min = as_date("2015-01-01"), max = as_date("2015-12-01"))),
+  tabsetPanel(
+    tabPanel("Map", 
+             mainPanel(leafletOutput("map"))
+             ),
     
-    mainPanel(
-      plotOutput("plot"), textOutput("source"), uiOutput("link"), uiOutput("link2")
+    tabPanel("Plot", 
+             sidebarLayout(
+               sidebarPanel(
+                 checkboxGroupInput(inputId = "damtype",
+                                    label = "Dam use",
+                                    choices = levels(dam2015fin2$purpose),
+                                    selected = levels(dam2015fin2$purpose)),
+                 dateRangeInput(inputId = "dates",
+                                label = "Select date range",
+                                start = as_date("2015-01-01"), end = as_date("2015-12-01"),
+                                min = as_date("2015-01-01"), max = as_date("2015-12-01"))),
+               mainPanel(plotOutput("plot"))
+             )),
+             
+    tabPanel("Sources",
+             mainPanel(
+               uiOutput("link"), uiOutput("link2")
+             )
     )
   )
 )
 
-
+    
+    
+    
 # Server function
 server <- function(input, output){
 
   
   dam2015react <- reactive({
     dam2015fin2 %>%
-      mutate(eff_sto_cap_106m3 = effective_storage_capacity_103m3/1000) %>%
+      mutate(eff_sto_cap_109m3 = effective_storage_capacity_103m3/1000000) %>%
       group_by(reservoir_name) %>%
       filter(purpose %in% input$damtype, 
              date <= input$dates[2], date >= input$dates[1])
@@ -51,11 +65,11 @@ server <- function(input, output){
   
       ggplot(dam2015react(),
              aes(x = date, y = value, color = purpose,
-                 size = eff_sto_cap_106m3)) +
+                 size = eff_sto_cap_109m3)) +
       geom_point(alpha = 0.6) +
       theme_minimal() +
-      labs(y = bquote("Water storage"~(10^6~m^3)), color = "Use",
-           size = bquote("Effective storage capacity"~(10^6~m^3)),
+      labs(y = bquote("Water storage"~(10^9~m^3)), color = "Use",
+           size = bquote("Effective storage capacity"~(10^9~m^3)),
            caption = "Data are at the monthly level.") +
       theme(axis.title.x = element_blank()) +
       guides(color = guide_legend(order = 1),
@@ -67,10 +81,6 @@ server <- function(input, output){
                          values = c("#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3"))
   })
 
-  
-  output$source <- renderText({
-    "Data sources:"
-  })
   
   
   output$link <- renderUI({
